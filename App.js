b50a53ad3929a60fd24a14d5d1990863bd594030
Taofiq Aiyelabegan <61933245+Taofiqq@users.noreply.github.com>
2022-06-16 10:17:56 +0800

    project bootstrapping

---
+++ App.js
@@ -0,0 +1,26 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text
+        style={{
+          color: "white",
+        }}
+      >
+        Open up App.js to start working on your app!
+      </Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#141414",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
---
+++ components/OTP/OTPInput.js
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+import React from "react";
+
+const OTPInput = () => {
+  return (
+    <View>
+      <Text>OTPInput</Text>
+    </View>
+  );
+};
+
+export default OTPInput;
